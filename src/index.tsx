bcf26fd9c68678a0602f0f11fb274a4e0aecae78
sinafath <83284138+sinafath@users.noreply.github.com>
2023-07-22 11:45:52 +0000

    init

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+ type fetchArg<returnedValue> = (subString: string) => Promise<returnedValue>;
+function sina<returnedValue>(fetchArg:fetchArg<returnedValue>) {
+    
+    return null
+}
+export default sina
